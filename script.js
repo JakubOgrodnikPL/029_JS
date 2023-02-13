6828ec69d80f7aed5bf7ef81046fa7013f52999b
Jakub Ogrodnik <81594386+JakubOgrodnikPL@users.noreply.github.com>
2023-02-13 13:25:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('button')
+// console.log(btn.outerHTML)
+// console.log(btn.innerHTML)
+// btn.outerHTML = '123'
+// btn.innerHTML = '123'
+// btn.innerHTML = '<li>123</li>'
+console.log(btn.innerText)
+console.log(btn.textContent)
+
+// btn.textContent = 123
+// btn.innerText = '123'
+
+
